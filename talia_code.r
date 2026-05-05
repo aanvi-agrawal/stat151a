22addd983ca59dd6736355ef60f62197b7edb901
taliamnguyen <talianguyen@berkeley.edu>
2026-05-04 18:33:36 -0700

    Update talia_code.r

--- talia_code.r
+++ talia_code.r
@@ -1,3 +1,27 @@
+```{r}
+model_all <- lm(SalePrice ~ Bedroom.AbvGr + Total.Bath +
+              Gr.Liv.Area + Overall.Qual +
+              Year.Built + Neighborhood +
+              Bedroom.AbvGr * Gr.Liv.Area +
+              Total.Bath * Gr.Liv.Area + 
+              Bedroom.AbvGr * Overall.Qual +
+              Total.Bath * Overall.Qual +
+              Bedroom.AbvGr * Year.Built +
+              Total.Bath * Year.Built +
+              Bedroom.AbvGr * Neighborhood + 
+              Total.Bath * Neighborhood +
+              Gr.Liv.Area * Overall.Qual +
+              Overall.Qual * Year.Built +
+              Neighborhood * Overall.Qual +
+              Bedroom.AbvGr * Total.Bath
+              ,
+            data = ames)
+summary(model_all)
+```
+# the above is not great - wide confidence interval
+
+
+```{r}
 model_quad <- lm(SalePrice ~ 
                    Bedroom.AbvGr + I(Bedroom.AbvGr^2) +
                    Total.Bath + I(Total.Bath^2) +
@@ -5,3 +29,9 @@ model_quad <- lm(SalePrice ~
                  data = ames)
 
 summary(model_quad)
+```
+# maybe build off of this one?
+-
+# this one is interesting because it shows that as you add more bedrooms, 
+# the price reduction increases but as you add more bathrooms, your price 
+# actually increases

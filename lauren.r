fce74dfc594ac220568d98ce564d198215133015
lauren-the-great <laurenmk@berkeley.edu>
2026-05-08 22:17:17 -0700

    Update lauren.r

--- lauren.r
+++ lauren.r
@@ -111,4 +111,44 @@ ggplot(ames1, aes(x = Bedroom.AbvGr, y = SalePrice)) +
   ) +
   theme_bw()
 ```
+```{r}
+ames2 <- ames1 |>
+  mutate(
+    date = as.Date(paste(as.character(Yr.Sold), as.character(Mo.Sold), "01", sep = "-")),
+    log_price = log(SalePrice),
+    Total.Bath = Full.Bath + 0.5 * Half.Bath +
+                 Bsmt.Full.Bath + 0.5 * Bsmt.Half.Bath
+  ) |>
+  filter(Gr.Liv.Area <= 4000)
 
+num_knots <- 5
+quants <- 1:num_knots / (num_knots + 1)
+knots3 <- quantile(ames2$Total.Bath, probs = quants, na.rm = TRUE)
+regs3 <- bSpline(
+  ames2$Total.Bath,
+  knots = knots3,
+  degree = 0
+)
+
+x3 <- seq(
+  min(ames2$Total.Bath, na.rm = TRUE),
+  max(ames2$Total.Bath, na.rm = TRUE),
+  length.out = 100
+)
+
+newdata3 <- data.frame(Total.Bath = x3)
+
+lmBath_Sale_fit <- lm(SalePrice ~ bSpline(Total.Bath, df=4, degree=2), ames2)
+
+yhat3 <- predict(lmBath_Sale_fit, newdata = newdata3)
+
+ggplot(ames2, aes(x = Total.Bath, y = SalePrice)) +
+  geom_point(alpha = 0.3) +
+  geom_line(
+    data = data.frame(Total.Bath = x3, yhat = yhat3),
+    aes(x = Total.Bath, y = yhat),
+    color = "red",
+    linewidth = 1.6
+  ) +
+  theme_bw()
+```
